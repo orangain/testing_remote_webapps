3ae540cab045d46f623db0823c720ea444d021d4
orangain <orangain@gmail.com>
2012-11-13 20:42:23 +0900

    7. cucumberを実行する, 8. step_definitionを作成する

---
+++ features/step_definitions/search_steps.rb
@@ -0,0 +1,21 @@
+#coding: utf-8
+
+Given /^I am on the "(.*?)" page of Google$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I search for "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should get response with content\-type "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see "(.*?)" in the title bar$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see "(.*?)" in the page$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
